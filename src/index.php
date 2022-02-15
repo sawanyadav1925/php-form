0017f74b88c88d4a80169940542df86cb809b3be
sawanyadav1925 <sawanyadav@cedcoss.com>
2022-02-15 18:23:30 +0530

    Conversion of time

--- src/index.php
+++ src/index.php
@@ -1,27 +1,53 @@
 <?php
-if(isset($_POST['submit']))  
-    {    
-$length = $_POST['length']; 
-$width = $_POST['width'];    
-$area = $length*$width; 
-$perimeter=2*($length+$width);  
-$a = "Area is ".$area."sq.mtr";
-$b = " Perimeter is ".$perimeter."mtr";
-}   
-?> 
-<html>
-    <head>
-        <title>area</title>
-    </head>  
-<body> 
-<form method = "post">      
-Length: <input type="number" name="length"> <br> <br>
-Width: <input type="number" name="width"> <br><br>  
-<input type = "submit" name = "submit" value="Area & Perimeter">   
-</form>  
-<div>
-<p>  <?php echo $a?> </p>
-<p>  <?php echo $b?></p>
-</div> 
-</body>   
-</html>  
+$user_Input=$_POST['user input'];
+$time= $_POST['time'];
+$result;
+
+if(isset($_POST['convert'])){
+print_r($_POST);
+    if($time=="htom"){
+$result=60*$user_Input." min";
+}
+
+elseif($time=="htos"){
+$result= (60*60)*$user_Input." seconds";
+}
+
+}
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Conversion</title>
+</head>
+<body>
+<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
+
+
+<input type="text" name="user input"><br><br>
+
+<input type="radio" id="" name="time" value="htom">
+<label for="">Hours to Min</label><br>
+
+<input type="radio" id="" name="time" value="htos">
+<label for="">Hours to Sec</label><br>
+
+<input type="submit" name="convert" value="convert">
+
+
+<p>
+    <?php
+    echo $result;
+    ?>
+</p>
+
+</form>
+
+  
+</body>
+</html>
