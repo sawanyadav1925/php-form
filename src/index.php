1c5cedbb0e79629897bd120cb9d06f4c9fcc3208
sawanyadav1925 <sawanyadav@cedcoss.com>
2022-02-15 18:55:05 +0530

    Upload Png file

--- src/index.php
+++ src/index.php
@@ -1,53 +1,60 @@
 <?php
-$user_Input=$_POST['user input'];
-$time= $_POST['time'];
-$result;
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-if(isset($_POST['convert'])){
-print_r($_POST);
-    if($time=="htom"){
-$result=60*$user_Input." min";
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
 }
 
-elseif($time=="htos"){
-$result= (60*60)*$user_Input." seconds";
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
 }
 
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 2048000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if( $imageFileType != "png")  {
+  echo "Sorry only PNG files are allowed.<br>";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
 }
 ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Conversion</title>
-</head>
+<html>
 <body>
-<form action="<?php echo $_SERVER['PHP_SELF']?>" method="post">
 
-
-<input type="text" name="user input"><br><br>
-
-<input type="radio" id="" name="time" value="htom">
-<label for="">Hours to Min</label><br>
-
-<input type="radio" id="" name="time" value="htos">
-<label for="">Hours to Sec</label><br>
-
-<input type="submit" name="convert" value="convert">
-
-
-<p>
-    <?php
-    echo $result;
-    ?>
-</p>
-
+<form action="" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
 </form>
 
-  
 </body>
 </html>
