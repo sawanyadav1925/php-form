90165828cda8f126d68c9d3fd2457bb311515b74
sawanyadav1925 <sawanyadav@cedcoss.com>
2022-02-15 17:41:50 +0530

    Area and Perimeter

--- src/index.php
+++ src/index.php
@@ -1,49 +1,27 @@
 <?php
-$Number_1 = $_POST['num1'];
-$Number_2 = $_POST['num2'];
-$operator = $_POST['operator'];
-$result = '';
-if (is_numeric($Number_1) && is_numeric($Number_2)) {
-    switch ($operator) {
-        case "+":
-           $result = $Number_1 + $Number_2;
-            break;
-        case "-":
-           $result = $Number_1 - $Number_2;
-            break;
-        case "*":
-            $result = $Number_1 * $Number_2;
-            break;
-        case "/":
-            $result = $Number_1 / $Number_2;
-    }
-}
-
-
-?>
-<!DOCTYPE html>
-
-<head>
-	<title>calculator</title>
-</head>
-
-<body>
-    <div id="wrapper">
-	  <form action="" method="post">
-            <p>
-                Number1:&emsp;<input type="number" name="num1" id="num1"  value="<?php echo $Number_1; ?>" /> <b><b>
-            </p>
-            <p>
-                Number2:&emsp;<input type="number" name="num2" id="num2"  value="<?php echo $Number_2; ?>" /> <b><b>
-            </p>
-            <p>
-            Result:&emsp;<input readonly="readonly" name="result" value="<?php echo $result; ?>"> <b><b>
-            </p>
-            <input type="submit" name="operator" value="+" />
-            <input type="submit" name="operator" value="-" />
-            <input type="submit" name="operator" value="*" />
-            <input type="submit" name="operator" value="/" />
-	  </form>
-    </div>
-</body>
-</html>
+if(isset($_POST['submit']))  
+    {    
+$length = $_POST['length']; 
+$width = $_POST['width'];    
+$area = $length*$width; 
+$perimeter=2*($length+$width);  
+$a = "Area is ".$area."sq.mtr";
+$b = " Perimeter is ".$perimeter."mtr";
+}   
+?> 
+<html>
+    <head>
+        <title>area</title>
+    </head>  
+<body> 
+<form method = "post">      
+Length: <input type="number" name="length"> <br> <br>
+Width: <input type="number" name="width"> <br><br>  
+<input type = "submit" name = "submit" value="Area & Perimeter">   
+</form>  
+<div>
+<p>  <?php echo $a?> </p>
+<p>  <?php echo $b?></p>
+</div> 
+</body>   
+</html>  
